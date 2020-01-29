84bf6d77307c388291d4637811b2df522e6cb509
Lartos <60252312+Lartos-repo@users.noreply.github.com>
2020-01-29 11:03:10 +0100

    Basic structure and finished css

---
+++ viewer.js
@@ -0,0 +1,5 @@
+
+var nummer = "Lartos_";
+document.getElementById("insert").innerHTML = nummer;
+document.getElementById("h2").innerHTML = Descr;
+document.getElementById("link").href = "https://www.twitch.tv/"+nummer;
