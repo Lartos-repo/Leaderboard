88d4077412eb192270e7c792d3eba842613401d9
Lartos <60252312+Lartos-repo@users.noreply.github.com>
2020-01-30 09:30:36 +0100

    Add files via upload

--- viewer.js
+++ viewer.js
@@ -1,5 +1,5 @@
 
 var nummer = "Lartos_";
-document.getElementById("insert").innerHTML = nummer;
+document.getElementById("a").innerHTML = nummer;
 document.getElementById("h2").innerHTML = Descr;
 document.getElementById("link").href = "https://www.twitch.tv/"+nummer;
